1e66da5e7ea457369138ac84b32b3d2645c2eb16
Trumeet <liangyuteng12345@gmail.com>
2017-08-27 20:43:24 +0800

    feat: add 8dp elevation on bottom bar

--- app/src/main/java/top/trumeet/mipushframework/settings/MainActivity.java
+++ app/src/main/java/top/trumeet/mipushframework/settings/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -91,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        ViewCompat.setElevation(bottomNavigationView, 8f);
         viewPager.setCurrentItem(1);
     }
 
