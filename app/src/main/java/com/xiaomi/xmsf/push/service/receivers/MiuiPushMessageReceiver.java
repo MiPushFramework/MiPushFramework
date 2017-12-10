e27c27ddfccdb9e15fa9b7590a07c18fd61d1d89
Trumeet <liangyuteng12345@gmail.com>
2017-12-10 14:28:55 +0800

    feat: revert MiuiPushMessageReceiver

--- app/src/main/java/com/xiaomi/xmsf/push/service/receivers/MiuiPushMessageReceiver.java
+++ app/src/main/java/com/xiaomi/xmsf/push/service/receivers/MiuiPushMessageReceiver.java
@@ -1,10 +1,50 @@
 package com.xiaomi.xmsf.push.service.receivers;
 
+import android.content.Context;
+import android.content.Intent;
+
+import com.xiaomi.mipush.sdk.MiPushCommandMessage;
+import com.xiaomi.mipush.sdk.MiPushMessage;
 import com.xiaomi.mipush.sdk.PushMessageReceiver;
+import com.xiaomi.xmsf.push.service.MyLog;
+import com.xiaomi.xmsf.push.service.XMAccountManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MiuiPushMessageReceiver extends PushMessageReceiver {
-    // We needn't do anything. This receiver is never called.
-    // XMPush will auto send broadcast to target app.
-    // We use Condom to check push request.
-    // This receiver is only use to pass XMPush manifest check.
+    private final Logger logger = LoggerFactory.getLogger(MiuiPushMessageReceiver.class);
+
+    public void onCommandResult(Context context, MiPushCommandMessage miPushCommandMessage) {
+        logger.debug("onCommandResult");
+        if (miPushCommandMessage.getResultCode() == 0) {
+            String command = miPushCommandMessage.getCommand();
+            if (miPushCommandMessage.getCommandArguments().size() > 0 && "register".equals(command)) {
+                XMAccountManager.getInstance(context).setAccountAsAlias();
+                return;
+            }
+            return;
+        }
+        MyLog.m17e(miPushCommandMessage.toString());
+    }
+
+    public void onReceiveMessage(Context context, MiPushMessage miPushMessage) {
+        logger.info("onReceiveMessage -> " + miPushMessage.toString());
+        String str = (String) miPushMessage.getExtra().get("miui_package_name");
+        if (str != null && !str.trim().isEmpty()) {
+            logger.debug("not empty");
+            Intent intent = new Intent();
+            intent.setPackage(str);
+            intent.putExtras(miPushMessage.toBundle());
+            if (miPushMessage.isNotified()) {
+                logger.debug("isNotified -> true");
+                intent.setAction("com.xiaomi.mipush.miui.CLICK_MESSAGE");
+                context.startService(intent);
+                return;
+            }
+            logger.debug("send broadcast");
+            intent.setAction("com.xiaomi.mipush.miui.RECEIVE_MESSAGE");
+            context.sendBroadcast(intent);
+        }
+    }
 }
