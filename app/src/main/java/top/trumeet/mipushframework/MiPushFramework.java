587eb6cc3c7f61d0a289d3a8d04cdf118451ae68
Trumeet <liangyuteng12345@gmail.com>
2018-01-31 15:15:38 +0800

    feat: fabric analytics

--- app/src/main/java/top/trumeet/mipushframework/MiPushFramework.java
+++ app/src/main/java/top/trumeet/mipushframework/MiPushFramework.java
@@ -2,6 +2,11 @@ package top.trumeet.mipushframework;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+import top.trumeet.mipush.BuildConfig;
+
 /**
  * Created by Trumeet on 2017/12/23.
  */
@@ -10,7 +15,12 @@ public class MiPushFramework extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        if (BuildConfig.DEBUG) {
+            final Fabric fabric = new Fabric.Builder(this)
+                    .kits(new Crashlytics())
+                    .build();
+            Fabric.with(fabric);
+        }
     }
 
     @Override
