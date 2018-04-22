6ba6d717b60567f21f000fef45ded1ae140a3aa5
Trumeet <liangyuteng12345@gmail.com>
2018-04-22 19:57:37 +0800

    feat(push): flush log before sharing

--- push/src/main/java/com/xiaomi/xmsf/ShareLogActivity.java
+++ push/src/main/java/com/xiaomi/xmsf/ShareLogActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.xiaomi.xmsf.utils.LogUtils;
 
+import me.pqpo.librarylog4a.Log4a;
+
 /**
  * Created by Trumeet on 2017/12/29.
  */
@@ -13,6 +15,7 @@ public class ShareLogActivity extends Activity {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log4a.flush();
         LogUtils.shareFile(this);
         finish();
     }
