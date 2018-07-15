7b3c969ef46bce7deeb6b3273e12aa0ca8cbff4a
Trumeet <liangyuteng12345@gmail.com>
2018-07-15 15:15:49 +0800

    feat(common): finish H2OS checker

--- common/src/main/java/top/trumeet/common/utils/rom/h2os/H2OSChecker.java
+++ common/src/main/java/top/trumeet/common/utils/rom/h2os/H2OSChecker.java
@@ -5,7 +5,13 @@ import top.trumeet.common.utils.rom.RomChecker;
 public class H2OSChecker implements RomChecker {
     @Override
     public boolean check() {
-        // TODO: Add H2OS checking
-        return false;
+        try {
+            // 可能是只有 H2OS 才有的类（
+            Class opFeatures = Class.forName("com.oneplus.sdk.utils.OpFeatures");
+            System.out.println(opFeatures);
+            return true;
+        } catch (Throwable e) {
+            return false;
+        }
     }
 }
