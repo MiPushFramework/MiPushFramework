99d332a6903acc6e7d3af39181e18b03f5d81151
Trumeet <liangyuteng12345@gmail.com>
2018-07-27 12:32:31 +0800

    feat(manager): collect some information

--- app/src/main/java/top/trumeet/mipushframework/MiPushFramework.java
+++ app/src/main/java/top/trumeet/mipushframework/MiPushFramework.java
@@ -6,10 +6,14 @@ import android.content.SharedPreferences;
 import com.crashlytics.android.Crashlytics;
 import com.crossbowffs.remotepreferences.RemotePreferenceAccessException;
 
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 import io.fabric.sdk.android.Fabric;
 import rx_activity_result2.RxActivityResult;
 import top.trumeet.common.utils.PreferencesUtils;
 import top.trumeet.mipush.BuildConfig;
+import top.trumeet.mipushframework.debug.IdJob;
 import top.trumeet.mipushframework.utils.BaseAppsBinder;
 
 /**
@@ -25,6 +29,12 @@ public class MiPushFramework extends Application {
                     .kits(new Crashlytics())
                     .build();
             Fabric.with(fabric);
+            // 用于判断部分 ROM 是不是把 IdJob 给拦截了
+            Crashlytics.getInstance().core.setBool("COLLECT_ENABLE", true);
+            // 收集信息
+            OneTimeWorkRequest request = OneTimeWorkRequest.from(IdJob.class).get(0);
+            WorkManager.getInstance().beginUniqueWork("Collect",
+                    ExistingWorkPolicy.KEEP, request).enqueue();
         }
 
         init();
