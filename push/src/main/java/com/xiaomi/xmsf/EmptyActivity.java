e65fd1f3dda1c44c5eec78ae8838357a2f6e7f33
Trumeet <liangyuteng12345@gmail.com>
2018-12-16 10:23:08 -0800

    feat(manager/push): simplify keep alive UI

--- push/src/main/java/com/xiaomi/xmsf/EmptyActivity.java
+++ push/src/main/java/com/xiaomi/xmsf/EmptyActivity.java
@@ -1,15 +1,7 @@
 package com.xiaomi.xmsf;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.widget.Toast;
-
-import top.trumeet.common.Constants;
-
-import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
 
 /**
  * @author zts
@@ -21,7 +13,6 @@ public class EmptyActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_empty);
-        moveTaskToBack(false);
     }
 
     public static class OnePlus extends EmptyActivity {
