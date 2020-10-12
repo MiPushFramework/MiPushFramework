1763fbfc1f4ed4ee3e13909aaff56149ffd519c5
zts1993 <zts1993@gmail.com>
2020-10-12 14:44:09 +0800

    hook by hibeaver
    Signed-off-by: zts1993 <zts1993@gmail.com>

---
+++ push/src/main/java/top/trumeet/mipushframework/Hook.java
@@ -0,0 +1,11 @@
+package top.trumeet.mipushframework;
+
+import android.util.Log;
+
+public class Hook {
+
+    public static void hookXM(Object checkThisBro) {
+        Log.i("hookXM", "hooking: " + checkThisBro.toString());
+    }
+
+}
